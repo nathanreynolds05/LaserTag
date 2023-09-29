6886675e3ca5ed73f936fdcbc32d0737359f304c
nathanreynolds05 <145401064+nathanreynolds05@users.noreply.github.com>
2023-09-28 21:33:45 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,16 @@ public class Main
 {
 	public static void main(String[] args) throws IOException
 	{
+		//Create a SplashScreen Object
 		SplashScreen splashScreen = new SplashScreen();
+
+		//Creates an EntryScreen Object
 		EntryScreen entryScreen = new EntryScreen();
+
+		//Creates a DataBaseConnector Object
+		DatabaseConnector database = new DatabaseConnector("jdbc:postgresql://db.gkeebcixaqkelmnqriql.supabase.co:5432/postgres",
+		"postgres", "UA7ashu40JCkUNHwYymL");
+
 		try {
             // Make photon image appear for 3 seconds before disappearing
             Thread.sleep(3000);
@@ -15,9 +23,19 @@ public class Main
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+		//Connect to the server
 		udpBaseServer_2.baseServer();
+
+		//Connect to the database
+		database.connect();
+
 	}
 
 
 }
+
+//To Do list:
+//1. We need to figure out how to connect the database with the entryscreen.
+//2. Need to find a way to delay the entry screen until after the splashscreen.
+
+
