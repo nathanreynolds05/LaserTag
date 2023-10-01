fde0f1995aa76b80f99f2fef7718f01a380c473e
Dylan Baker <144173996+dab023@users.noreply.github.com>
2023-09-30 22:58:27 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.lang.String;
 
 public class Main 
 {
 	public static void main(String[] args) throws IOException
 	{
+        //Connect to the server
+		//udpBaseServer_2.baseServer();
+
 		//Create a SplashScreen Object
 		SplashScreen splashScreen = new SplashScreen();
-
-
-		//Creates a DataBaseConnector Object
-		DatabaseConnector database = new DatabaseConnector("jdbc:postgresql://db.gkeebcixaqkelmnqriql.supabase.co:5432/postgres",
-		"postgres", "UA7ashu40JCkUNHwYymL");
 
 		try {
             // Make photon image appear for 3 seconds before disappearing
@@ -25,24 +22,8 @@ public class Main
 		//Creates an EntryScreen Object
 		EntryScreen entryScreen = new EntryScreen();
 
-		//Connect to the server
-		udpBaseServer_2.baseServer();
-
-		//Connect to the database
-		database.connect();
-
-		// Player player1 = new Player(1, "Jimmy", "Neutron", "Alpha");
-		// database.addPlayer(player1);
-
-		// database.searchByID(1);
-		// database.searchByID(2);
-
-
+		
 	}
 
 
 }
-//To Do list:
-//1. We need to figure out how to connect the database with the entryscreen.
-
-
